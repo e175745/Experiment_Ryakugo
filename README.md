a6025d78ccad8d3c8ed4d9753bec9da4d959a9c5
Kazuma M <e175745@ie.u-ryukyu.ac.jp>
2019-07-31 19:33:15 +0900

    change README.md

--- README.md
+++ README.md
@@ -1,11 +1,40 @@
 # Anime Title Abbreviation
 =====
 ### 琉球大学工学部知能情報実験Ⅱ データマイニング班 G1
-## 概要
+## Description
 過去のアニメタイトルから未知のアニメタイトルの略称を予測する。
 タイトルを一文字づつベクトル化し、線形回帰を用いてた略称となるばベクトル群を求める。
 モジュールはsklearnに入っているLinearRegressionを使用した。
 
-## ファイル内容
+## File Contents
+### input.py
+データの取り込みや前処理に関する関数が書かれている。
+基本的にはchar_vec_sklearn.pyでモジュールとしてインポートし、使用する。
 
-## 開発環境
+### char_vec_sklearn.py
+input.pyにある関数を用いて学習を行う。
+基本的にはただ実行するだけで学習が行われる。
+また、学習とともにモデルの保存や精度の計算を行う。
+
+### exe_Ryaku.py
+保存されたモデルを用いて未知のタイトルに対して略称を予測する。
+下記のように実行することで起動可能。
+% python3 exe_Ryaku.py Linear_Regression.sav
+引数には保存したモデルの名前を渡してやる。
+指示に従って予測してほしいタイトルを入力すると学習機によって予測された略称が出てくる。
+
+## Git URL
+https://github.com/e175745/Experiment_Ryakugo.git
+
+## About Datasets
+今回使用したデータセットについては、Webページに記載されている情報をウェブスクレイピングによって収集しcsvファイルに直したものを使用した。収集したデータの再配布はあまり良くないのでここでは公開しない。
+
+## Development Environment
+macOS Mojave
+python 3.7.2
+sklearn 0.20.3
+janome 0.3.9
+
+## Author
+氏名 : 松本 一馬
+連絡先 : e175745@ie.u-ryukyu.ac.jp
--- char_vec_sklearn.py
+++ char_vec_sklearn.py
@@ -1,75 +1,22 @@
 # coding UTF-8
 from input import read_file, conv_str_to_kana, conv_kana_to_vec, conv_vec_to_kana, calc_accuracy
-from sklearn import svm
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import LeaveOneOut
-from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_absolute_error
-from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import csv
 import pickle
 
-#data = read_file('dataset_for.csv')
-data = read_file('abbrs_title.csv')
+data = read_file('dataset_for.csv')
+#data = read_file('abbrs_title.csv')
 kana_title = conv_str_to_kana(data[0])
 kana_ans = conv_str_to_kana(data[1])
 #vec_title = conv_kana_to_vec(kana_title,1,"T")
 #vec_ans = conv_kana_to_vec(kana_ans,2,"R")
 vec_title = conv_kana_to_vec(kana_title,1,"T")
 vec_ans = conv_kana_to_vec(kana_ans,1,"R")
-"""
-# 相関関係ヒートマップ
-sns.set()
-
-vec_title = np.array(vec_title)
-corr = np.corrcoef(vec_title.transpose(), vec_title)
-print(corr)
-sns.heatmap(corr, annot=True)
-plt.show()
-"""
-
-"""
-for i,kana_title in enumerate(kana_title):
-    print(kana_title)
-    print(vec_title[i])
-
-clf = svm.SVC(gamma=0.001, C=100)
-clf.fit(vec_title[:-1], vec_ans[:-1])
-
-result = clf.predict(data[:-1])
-print("実際の答え={0}, 予測結果={1}".format(vec_ans[-1], result))
-
-result = clf.score(data, vec_ans)
-print(result)
-"""
-
-"""
-# データセットを学習用とテスト用に分割
-X_train, X_test, Y_train, Y_test = train_test_split(vec_title, vec_ans, train_size = 0.8, test_size = 0.2, random_state = 1)
-
-# 線形回帰に適用
-lr = LinearRegression()
-lr.fit(X_train, Y_train)
-Y_pred = lr.predict(X_test)
-
-Y_pred = np.array(Y_pred)
-Y_test = np.array(Y_test)
-
-# 予測と正解の差異を計算
-pred_diff = []
-for j,title in enumerate(Y_pred):
-    title_diff = []
-    for k,val in enumerate(title):
-        diff = abs(val - Y_test[j,k])
-        title_diff.append(diff)
-    pred_diff.append(title_diff)
-
-print(mean_absolute_error(Y_test, Y_pred))
-"""
 
 loo = LeaveOneOut()
 lr = LinearRegression()
@@ -124,15 +71,6 @@ for title in result:
             title[index] = 0
         else:
             title[index] = 1
-        """
-        ind = abs(round(i))
-        if index%2==0 and ind >= 6:
-            title[index] = 5
-        elif ind > 20 :
-            title[index] = 20
-        else :
-            title[index] = ind
-        """
 
 # カナに直して比較
 # Y_pred = Y_pred.tolist()
